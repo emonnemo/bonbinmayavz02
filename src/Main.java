56ff84d035c412e7023450f03ef26b4a3368c310
Andika Kusuma <Andika Kusuma>
2017-03-27 18:38:08 +0700

    driver dikit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import driver.Driver;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Driver a;
+        a = new Driver();
     }
 }
