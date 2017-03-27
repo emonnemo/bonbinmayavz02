6ca86d1ba15e87b7e7de52cf96d7ca607681edf9
Gisela <giselasupardi@gmail.com>
2017-03-27 23:19:14 +0700

    cell lagi

--- src/cell/Cell.java
+++ src/cell/Cell.java
@@ -14,7 +14,7 @@ public class Cell {
   private char symbol; /**< simbol yang akan ditampilkan pada layar (dapat dioverwrite animal)*/
   private boolean sekat[4]; /**< state dari sekat untuk habitat; true untuk terbuka, false untuk tertutup */
   private StringBuffer plant; /**< tanaman yang ditanam di taman*/
-  private string food; /**< jenis makanan yang dijual di restoran*/
+  private StringBuffer food; /**< jenis makanan yang dijual di restoran*/
   private boolean open; /**< penanda restoran buka atau tutup; true untuk buka, false untuk tutup*/
 
   /**
@@ -22,34 +22,38 @@ public class Cell {
    * \details Menciptakan cell dengan symbol c.
    * \param c symbol input.
    */
-  public Cell(char c);
+  public Cell(char c) {}
 
   /**
    * \brief GetInitSymbol.
    * \details membalikan nilai initial_symbol cell.
    * \return init symbol.
    */
-  public char GetInitSymbol();
+  public char GetInitSymbol() {
+    return initial_symbol;
+  }
 
   /**
    * \brief GetSymbol.
    * \details mengembalikan nilai simbol cell.
    * \return symbol.
    */
-  public char GetSymbol();
+  public char GetSymbol() {
+    return symbol;
+  }
 
   /**
    * \brief SetSymbol.
    * \details mengubah symbol dengan s.
    * \param s symbol yang baru.
    */
-  public void SetSymbol(char s);
+  public void SetSymbol(char s) {}
 
   /**
    * \brief Render
    * \details Menampilkan representasi cell pada layar
    */
-  public void Render();
+  public void Render() {}
 
   /**
    * \brief ToggleSekat
@@ -58,7 +62,7 @@ public class Cell {
    *
    * \param direction arah yang ingin dibuka
    */
-  public void ToggleSekat(int direction);
+  public void ToggleSekat(int direction) {}
 
   /**
    * \brief GetSekat
@@ -67,52 +71,60 @@ public class Cell {
    *
    * \param direction arah yang ingin dicek
    */
-  public boolean GetSekat(int direction);
+  public boolean GetSekat(int direction) {
+    return sekat[direction];
+  }
 
   /**
    * \brief Interact
    * \details Interaksi dengan Cell; tidak diimplementasi pada class ini
    */
-  public void Interact();
+  public void Interact() {}
 
   /**
    * \brief GetFood
    * \details Mengembalikan jenis makanan yang dijual restoran
    * \return string
    */
-  public StringBuffer GetFood();
+  public StringBuffer GetFood() {
+    return food;
+  }
 
   /**
    * \brief GetOpen
    * \details Mengembalikan status restoran
    * \return bool true jika buka, false jika tutup
    */
-  public boolean GetOpen();
+  public boolean GetOpen() {
+    return open;
+  }
 
   /**
    * \brief SetFood
    * \details Mengubah jenis makanan yang dijual restoran
    * \param f Jenis makanan yang akan dijual
    */
-  public void SetFood(string f);
+  public void SetFood(string f) {}
 
   /**
    * \brief ToggleOpen
    * \details Mengubah status restoran
    */
-  public void ToggleOpen();
+  public void ToggleOpen() {}
 
   /**
    * \brief GetPlant
    * \details Mengembalikan tanaman yang tertanam pada taman
    * \return string
    */
-  public StringBuffer GetPlant() const;
+  public StringBuffer GetPlant() {
+    return plant;
+  }
 
   /**
    * \brief SetPlant
    * \details Mengubah tanaman yang tertanam pada taman dengan p
    * \param p Tanaman yang ingin ditanam
    */
-  public void SetPlant(string p);
+  public void SetPlant(string p) {}
 }
